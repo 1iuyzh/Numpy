68bd6364f3ca00ea4f1fafbf3104634b7e031f3f
liuyzh <liuyzh@tju.edu.cn>
2018-06-29 16:47:55 +0800

    tf

--- tensorflow/MNIST/input_data.py
+++ tensorflow/MNIST/input_data.py
@@ -13,45 +13,57 @@ def decode_idx_ubyte(path):
     buffer = open(path, 'rb').read()
     # 解析头信息
     offset = 0
-    head_format = '>i' # big-endian
+    head_format = '>i' # big-endian 字节序
     magic_number = struct.unpack_from(head_format, buffer, offset)[0]
-    if magic_number == 2049: # idx1
-        # print('magic number: %d' % magic_number)
-        offset += struct.calcsize(head_format)
+    offset += struct.calcsize(head_format)
+    if magic_number == 2049: # labels
         head_format = '>i'
         num_items = struct.unpack_from(head_format, buffer, offset)[0]
-        # print('number of items: %d' % num_items)
         # 解析数据集
-        offset += struct.calcsize(head_format)
+        offset += 4
         label_format = '>B'
         labels = np.empty(num_items)
         for i in range(num_items):
             labels[i] = struct.unpack_from(label_format, buffer, offset)[0]
-            offset += struct.calcsize(label_format)
-        return labels # (60000, 1)
-    if magic_number == 2051: # idx3
-        # print('magic number: %d' % magic_number)
-        offset += struct.calcsize(head_format)
-        head_format = '>iii'
+            offset += 1
+        return labels
+    if magic_number == 2051: # images
+        head_format = '>3i'
         num_images, num_rows, num_cols = struct.unpack_from(head_format, buffer, offset)
-        # print('number of images: %d, number of rows: %d, number of columns: %d' % (num_images, num_rows, num_cols))
         # 解析数据集
-        image_size = num_rows * num_cols
         offset += struct.calcsize(head_format)
+        image_size = num_rows * num_cols
         image_format = '>' + str(image_size) + 'B'
         images = np.empty([num_images, num_rows, num_cols])
         for i in range(num_images):
             images[i] = np.array(struct.unpack_from(image_format, buffer, offset)).reshape([num_rows, num_cols])
             offset += struct.calcsize(image_format)
-        return images # (60000, 28, 28)
+        return images
+
+def get_data():
+    images = decode_idx_ubyte(train_images_idx3_ubyte)
+    train_images = images.reshape([images.shape[0], -1]) / 255 # (60000, 784)
+
+    labels = decode_idx_ubyte(train_labels_idx1_ubyte)
+    train_labels = np.zeros([labels.shape[0], 10])
+    for i in range(labels.shape[0]):
+        train_labels[i][int(labels[i])] = 1 # (60000, 10)
+    
+    images = decode_idx_ubyte(test_images_idx3_ubyte)
+    test_images = images.reshape([images.shape[0], -1]) / 255 # (10000, 784)
+
+    labels = decode_idx_ubyte(test_lables_idx1_ubyte)
+    test_labels = np.zeros([labels.shape[0], 10])
+    for i in range(labels.shape[0]):
+        test_labels[i][int(labels[i])] = 1 # (10000, 10)
+    
+    return train_images, train_labels, test_images, test_labels
 
 # 测试
 def test():
     train_images = decode_idx_ubyte(train_images_idx3_ubyte)
     train_labels = decode_idx_ubyte(train_labels_idx1_ubyte)
-    print(train_images.shape)
-    print(train_labels.shape)
-    for i in range(10):
+    for i in range(3):
         print(train_labels[i])
         plt.imshow(train_images[i], cmap = 'gray')
         plt.show()
---
+++ tensorflow/MNIST/softmax.py
@@ -0,0 +1,49 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import input_data
+import random
+
+train_images, train_labels, test_images, test_labels = input_data.get_data()
+
+# model
+x = tf.placeholder('float', [None, 784])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+y = tf.nn.softmax(tf.matmul(x, W) + b) # tf.matmul
+
+# strategy
+y_ = tf.placeholder('float', [None, 10])
+cross_entropy = -tf.reduce_sum(y_ * tf.log(y)) # tf.reduce_sum
+
+# algorithm
+train = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+
+# initialization
+init = tf.global_variables_initializer()
+
+# run
+sess = tf.InteractiveSession()
+sess.run(init)
+
+# train
+for i in range(1000):
+    index = random.sample(range(train_images.shape[0]), 100)
+    batch_xs = train_images[index]
+    batch_ys = train_labels[index]
+    sess.run(train, feed_dict = {x: batch_xs, y_: batch_ys})
+
+# evaluation
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1)) # tf.equal
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, 'float')) # tf.cast
+print(sess.run(accuracy, feed_dict = {x: test_images[0:10], y_: test_labels[0:10]}))
+
+# verification
+result = tf.argmax(y, 1) # tf.argmax
+for i in range(3):
+    index = random.randint(0, test_images.shape[0] - 1)
+    label = sess.run(result, feed_dict = {x: test_images[index:index + 1], y_: test_labels[index:index + 1]}) # 不降维
+    print(label[0])
+    image = test_images[index].reshape(28, 28) * 255
+    plt.imshow(image, cmap = 'gray')
+    plt.show()
